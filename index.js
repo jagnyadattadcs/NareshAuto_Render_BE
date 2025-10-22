b54dd1617122945d9ed20d81d168873b53c44c82
jagnyadattadcs <raj90.dcs@gmail.com>
2025-10-22 17:10:00 +0530

    Added cors to all origin

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const exportRoutes = require('./routes/exportRoute');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 
 // Routes
